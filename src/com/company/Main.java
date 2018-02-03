2bb1abb37af51363200970613b521fc52ceb5a15
Highlevelyuri <kriucov61944zce3.com@gmail.com>
2018-02-03 09:52:12 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <20; j++)
+            {
+                System.out.print(" * ") ;
+            }
+            System.out.println();
+        }
+    }
+}
